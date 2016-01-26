158306fc0b635ae8eef91ec663821201916c473d
adia_a <Adia@Princess.local>
2016-01-26 11:36:12 -0600

    Can add timeslots

--- public/scripts/calendar-admin.js
+++ public/scripts/calendar-admin.js
@@ -1,10 +1,35 @@
-var app = angular.module('calendar-admin', ['mwl.calendar', 'ui.bootstrap']);
+var app = angular.module('calendar-admin', ['mwl.calendar', 'ui.bootstrap', 'ngRoute']);
 
-app.controller('adminCal', ['$scope','moment', 'calendarConfig',function($scope, moment, calendarConfig) {
+app.config(['$routeProvider', '$locationProvider',
+  function($routeProvider, $locationProvider) {
+    $routeProvider.
+    when('/admin/calendar', {
+      templateUrl: '../views/routes/adminCalendar.html',
+      controller: 'adminCal'
+    }).otherwise({
+      redirectTo: '/home'
+    });
+
+    $locationProvider.html5Mode(true);
+
+  }]);
+
+app.controller('adminCal', ['$scope','moment', 'calendarConfig', function($scope, moment, calendarConfig) {
+
+  var status = {
+    open: "Open Time Slot"
+
+  };
 
   $scope.view = 'month';
 
   $scope.viewDate = new Date(2016, 1, 1, 0);
+
+  $scope.submit = function(){
+    $scope.form.title = status.open;
+    $scope.form.type = 'info';
+    $scope.events.push($scope.form);
+  };
 
 
   $scope.events = [
@@ -12,13 +37,15 @@ app.controller('adminCal', ['$scope','moment', 'calendarConfig',function($scope,
       title: 'Event ID 1',
       type: 'info',
       startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16)
+      endsAt: new Date(2016, 1, 12, 16),
+      available: true
     },
     {
       title: 'Event ID 2',
       type: 'info',
       startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16)
+      endsAt: new Date(2016, 1, 12, 16),
+      available: false
     },
     {
       title: 'Event ID 3',
@@ -34,5 +61,34 @@ app.controller('adminCal', ['$scope','moment', 'calendarConfig',function($scope,
     }
   ];
 
+  $scope.isCellOpen = true;
+
+  $scope.eventClicked = function(event) {
+    if(event.available === true)
+      console.log('You can choose me!');
+    else{
+      console.log('sorry mate!')
+    }
+  };
 
 }]);
+
+/*
+app.factory('alert', function($uibModal) {
+
+  function show(action, event) {
+    return $uibModal.open({
+      templateUrl: 'modelContent.html',
+      controller: function ($scope) {
+        var vm = this;
+        $scope.action = action;
+        $scope.event = event;
+      },
+      controllerAs: 'vm'
+    });
+  }
+
+  return {
+    show: show
+  };
+});*/
--- server/routes/index.js
+++ server/routes/index.js
@@ -6,10 +6,10 @@ router.get('/', function(req, res) {
   var file = req.params[0] || '../public/views/index.html';
   res.sendFile(path.join(__dirname, '../', file));
 });
-
-router.get('/*', function(req, res) {
+/*
+router.get('/!*', function(req, res) {
  //'Path is: ', path.join(__dirname, '../', file);
   res.sendFile(path.join(__dirname, '../../public/views/index.html'));
-});
+});*/
 
 module.exports = router;

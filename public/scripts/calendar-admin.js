34f9aae9697e9b74f3d108641fa681cac6d093e8
adia_a <Adia@Princess.local>
2016-01-28 09:56:38 -0600

    Added layout for calendar. Working on states of buttons

--- public/scripts/calendar-admin.js
+++ public/scripts/calendar-admin.js
@@ -14,99 +14,102 @@ app.config(['$routeProvider', '$locationProvider',
 
   }]);
 
-app.controller('adminCal', ['$scope','moment', 'calendarConfig', '$uibModal', function($scope, moment, calendarConfig, $uibModal) {
+app.controller('adminCal', ['$scope','moment', 'calendarConfig', '$uibModal',
+  function($scope, moment, calendarConfig, $uibModal) {
 
-  var status = {
-    open: 'Open Time Slot'
+    var status = {
+      open: 'Open Time Slot'
+    };
 
-  };
-
-  $scope.view = 'month';
-
-  $scope.viewDate = new Date(2016, 1, 1, 0);
-
-  console.log($scope.events);
-
-  $scope.form = {
-
-  };
-
-  $scope.submit = function(){
-    $scope.form.title = status.open;
-    $scope.form.type = 'info';
-    $scope.form.startsAt = $scope.valuationDate;
-    $scope.events.push($scope.form);
+    $scope.view = 'month';
+    $scope.viewDate = new Date(2016, 1, 1, 0);
     console.log($scope.events);
 
-  };
+    $scope.form = {
+    };
 
-  $scope.events = [
-    {
-      title: 'Event ID 1',
-      type: 'info',
-      startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16),
-      available: true
+    $scope.submit = function() {
 
-    },
-    {
-      title: 'Event ID 2',
-      type: 'info',
-      startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16),
-      available: false
-    },
-    {
-      title: 'Event ID 3',
-      type: 'info',
-      startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16)
-    },
-    {
-      title: 'Event ID 4',
-      type: 'info',
-      startsAt: new Date(2016, 1, 12, 15),
-      endsAt: new Date(2016, 1, 12, 16)
-    },
-    {
-      title: 'Event ID 4',
-      type: 'info',
-      startsAt: new Date(2016, 1, 14, 15),
-      endsAt: new Date(2016, 1, 14, 16)
-    }
-  ];
+      $scope.form.title = status.open;
+      $scope.form.type = 'info';
+      $scope.form.startsAt = $scope.valuationDate;
+      $scope.form.startTime = $scope.mytime.toLocaleTimeString();
+      $scope.events.push($scope.form);
+      console.log($scope.events);
 
-  $scope.isCellOpen = true;
+    };
 
-  $scope.eventClicked = function(event) {
-    if(event.available === true)
-      console.log('You can choose me!');
-    else{
-      console.log('sorry mate!')
-    }
-  };
+    $scope.events = [
+      {
+        title: 'Event ID 1',
+        type: 'info',
+        startsAt: new Date(2016, 1, 12, 15),
+        endsAt: new Date(2016, 1, 12, 16),
+        available: true
+      },
+      {
+        title: 'Event ID 2',
+        type: 'info',
+        startsAt: new Date(2016, 1, 12, 15),
+        endsAt: new Date(2016, 1, 12, 16),
+        available: false
+      },
+      {
+        title: 'Event ID 3',
+        type: 'info',
+        startsAt: new Date(2016, 1, 12, 15),
+        endsAt: new Date(2016, 1, 12, 16)
+      },
+      {
+        title: 'Event ID 4',
+        type: 'info',
+        startsAt: new Date(2016, 1, 12, 15),
+        endsAt: new Date(2016, 1, 12, 16)
+      },
+      {
+        title: 'Event ID 4',
+        type: 'info',
+        startsAt: new Date(2016, 1, 14, 15),
+        endsAt: new Date(2016, 1, 14, 16)
+      }
+    ];
 
-  //$scope.valuationDate = '';
-  $scope.valuationDatePickerIsOpen = false;
+    $scope.isCellOpen = true;
 
-  $scope.valuationDatePickerOpen = function () {
+    $scope.eventClicked = function(event) {
+      if(event.available === true) {
+        console.log('You can choose me!');
+      } else {
+        console.log('sorry mate!');
+      }
+    };
 
-    $scope.valuationDatePickerIsOpen = true;
-  };
+    //$scope.valuationDate = '';
+    $scope.valuationDatePickerIsOpen = false;
 
+    $scope.valuationDatePickerOpen = function() {
+      $scope.valuationDatePickerIsOpen = true;
+    };
 
-  $scope.timespanClicked = function(date){
-    $scope.valuationDate = date;
+    $scope.timespanClicked = function(date) {
+      $scope.valuationDate = date;
+      //cell.cssClass = 'clicked-cell';
 
-    /*$uibModal.open({
-      templateUrl: 'addEventModal.html',
-      controller: 'adminCal',
-      size: 'lg',
-      scope: $scope
-      })*/
-  }
+      /*$uibModal.open({
+        templateUrl: 'addEventModal.html',
+        controller: 'adminCal',
+        size: 'lg',
+        scope: $scope
+        })*/
+    };
 
-}]);
+    //Timepicker logic
+    $scope.mytime = new Date();
+    $scope.hstep = 1;
+    $scope.mstep = 15;
+    $scope.ismeridian = true;
+
+  }]);
 
 /*
 app.factory('alert', function($uibModal) {
